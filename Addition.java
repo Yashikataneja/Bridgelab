fdbff487f1814198682ad391e5306776d32d935a
YashikaTaneja <yashika979.be22@chitkara.edu.in>
2025-06-30 12:05:31 +0530

    adding new file 2

---
+++ Addition.java
@@ -0,0 +1,25 @@
+/* PROGRAM OF ADDITION OF TWO NUMBERS */
+
+ import java.util.Scanner;
+
+public class Addition {
+
+    /* all the functionalities will start executing through main method only!
+    that is why the below line of code will show error bcz it is outside the main method
+     */
+    //System.out.println("hi");
+
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        int d = a - b;
+
+        System.out.println(c);
+        System.out.println(d);
+
+    }
+
+
+}
