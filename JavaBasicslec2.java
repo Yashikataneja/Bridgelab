cc37d7cfb3ae8104614cccae12a486d405e1e8a4
YashikaTaneja <yashika979.be22@chitkara.edu.in>
2025-07-01 10:07:11 +0530

    object of java class

---
+++ JavaBasicslec2.java
@@ -0,0 +1,20 @@
+
+public class JavaBasicslec2 {
+
+    public void myMethod(){
+        int x = 1;
+        int y = 2;
+        int q = x + y;
+        System.out.println(q);
+    }
+
+    public static void main(String[] args){
+        // Create an object of the class
+        JavaBasicslec2 obj = new JavaBasicslec2();
+
+        // Call the method using the object
+        obj.myMethod();
+    }
+
+}
+
