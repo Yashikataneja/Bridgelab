0fd861d1b0dc0a654e2f57cda4660e3ae9cf8647
YashikaTaneja <yashika979.be22@chitkara.edu.in>
2025-07-02 12:56:57 +0530

    adding cal2 file

---
+++ UsingScannerNotepad.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class UsingScannerNotepad
+{
+    public static void main(String[] args)
+    {
+
+        long roll_no;
+        String name;
+
+//System.out.println("hello");
+        Scanner sc=new Scanner(System.in);
+
+
+        System.out.println("Enter your Name : ");
+        name=sc.nextLine();
+        System.out.println("Enter your Roll_no: ");
+        roll_no=sc.nextLong();
+
+
+
+    }
+
+}
