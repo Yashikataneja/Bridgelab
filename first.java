acfc27853963e4d573969bcfc8130fbfdec68fd1
YashikaTaneja <yashika979.be22@chitkara.edu.in>
2025-07-03 11:00:32 +0530

    adding some more files

---
+++ first.java
@@ -0,0 +1,12 @@
+public class second
+{
+  public static void main(String[] args)
+{
+         //adding two numbers program
+         int a=3;
+         int b=2;
+         int c=a+b;
+   System.out.println(c);
+   return 0;
+}
+}
